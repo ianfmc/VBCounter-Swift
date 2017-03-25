187a51c59bc69f52016e9c85f7b8966a888c9021
Ian McCallum <ianfmc@gmail.com>
2017-03-25 11:09:48 -0700

    Initial Commit; button changes color of lable

--- VBCounter WatchKit Extension/ColorInterfaceController.swift
+++ VBCounter WatchKit Extension/ColorInterfaceController.swift
@@ -1,6 +1,6 @@
 //
-//  InterfaceController.swift
-//  VBCounter WatchKit Extension
+//  ColorInterfaceController.swift
+//  VBCounter
 //
 //  Created by Ian McCallum on 3/25/17.
 //  Copyright © 2017 com.jandm-design. All rights reserved.
@@ -10,22 +10,34 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class ColorInterfaceController: WKInterfaceController {
+    
+    @IBOutlet var colorLabel : WKInterfaceLabel!
+    
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+    @IBAction func changeColorToRed() {
+        colorLabel.setText("Red")
+        colorLabel.setTextColor(UIColor.red)
+    }
+    
+    @IBAction func changeColorToGreen() {
+        colorLabel.setText("Green")
+        colorLabel.setTextColor(UIColor.green)
+    }
 }
